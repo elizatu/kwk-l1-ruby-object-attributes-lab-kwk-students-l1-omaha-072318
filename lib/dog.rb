e3174ecf3e1da0875a7589981c316693dc095961
elizatu <eliza.turco@gmail.com>
2018-07-30 14:18:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # dog.rb
+
+
+# class Dog
+  
+#   def dog
+#     @name
+#   end
+  
+#   def name = (name)
+#     @name = name
+#   end 
+    
+# end
+
+class Dog 
+  def name=(dog_name)
+    @name = "Fido"
+  end
+  
+  def name
+    @name
+  end
+  
+def breed=(dog_breed)
+  @breed =
+end
+
+def 
+
+  
+
+
---
+++ squash.rb
@@ -0,0 +1,13 @@
+class ClassName
+  def method_name(parameter)
+    @class_Variable = parameter
+  end
+end
+
+class Dog 
+  public
+  def intialized(name, breed)
+    @name = name
+    @breed = breed
+   end
+end
